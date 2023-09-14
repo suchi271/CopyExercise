faa91893dba67dd18683779ad79e38d48c0dbad2
Suchitha Malisetty <suchithamalisetty@gmail.com>
2023-09-15 01:33:58 +0530

    project structure changed

--- script.ps1
+++
@@ -1,5 +0,0 @@
-# Define variables
-$resourceGroupName = "ResourceDeployment"
-
-# Deploy the Azure Storage Account using ARM template and parameters
-New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile azuredeploy.json -TemplateParameterFile azuredeploy.parameters.json
